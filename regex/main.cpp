e3956a526456d373e2c1ecfebe15aaa2b93a3a8c
ueberaccelerate <ueberaccelerate@gmail.com>
2020-02-27 16:37:47 +0300

    add gui (imgui + sfml)

--- regex/main.cpp
+++ regex/main.cpp
@@ -1,3 +1,12 @@
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
 #include <fstream>
 #include <iostream>
 #include <string_view>
@@ -53,13 +62,40 @@ void print_words(const std::string &data) {
     std::cout << e.what() << '\n';
   }
 }
-int main() {
-  std::string data = read_file("rename_1");
-  if( data.empty() ) {
-    std::cout << "empty data\n";
-    return -1;
-  }
-  print_words(data);
-  return 0;
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 }
 
