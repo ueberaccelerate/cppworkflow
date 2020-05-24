3dc373ea3be62dc2c33f97c11d5b7a24cfb40222
ueberaccelerate <ueberaccelerate@gmail.com>
2020-05-24 09:09:06 +0300

    -- added algorithms to workflow

---
+++ src/algorithms/include/cppworkflow/algorithms/sort.hpp
@@ -0,0 +1,12 @@
+#ifndef CPPWORKFLOW_ALGORITHMS_SORT_HPP
+#define CPPWORKFLOW_ALGORITHMS_SORT_HPP
+
+template <typename Container>
+void sort ( const Container& c ) {
+  if (c.empty()) {
+    return;
+  }
+
+}
+
+#endif // CPPWORKFLOW_ALGORITHMS_SORT_HPP
